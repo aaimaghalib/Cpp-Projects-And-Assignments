3430b539d697590a569102f989e223a4775aee77
aaimaghalib <aaimaghalib24@gmail.com>
2026-06-26 06:03:28 +0500

    Add files via upload
    Declare the variable named “Base”, “Height” and “Area” initialized by any value. Your program should calculate the Area of the Triangle.											
    Formula: 	Area of Triangle = (base*height)/2.

---
+++ Quiz # 1_Q # 3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>   
+using namespace std;
+int main()
+{
+  int base,height,area;
+  cout<<"Enter the value of base";
+  cin>>base;
+  cout<<"Enter the value of height";
+  cin>>height;
+  area=(base*height)/2;
+  cout<<"area of triangle"<<area<<endl;
+  return 0;	
+}
